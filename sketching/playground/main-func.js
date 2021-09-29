f33ddeba3b56272b274389246976e79cfd601b07
NourAybak <nooreldin3000@gmail.com>
2021-09-29 14:40:34 +0200

    add arbitrary data + matching test

--- sketching/playground/main-func.js
+++ sketching/playground/main-func.js
@@ -4,17 +4,28 @@ const lg = val => console.log(val)
 
 // temp data
 const dataFiles = [
-    {"Name":"Noureldin Ghasey","B-day": "2003-1-1"},
-    {"Name": "Youssef Elhosini","B-day": "2002-1-8"}
+    {"Name":"R1","B-day": "2001-5-1"},
+    {"Name":"R2","B-day": "2004-2-4"},
+    {"Name":"R3","B-day": "2000-4-7"},
+    {"Name":"R4","B-day": "2002-1-15"},
+    {"Name":"R5","B-day": "2003-3-30"},
+    {"Name":"R6","B-day": "1996-1-29"},
 ]
 
 
 // start here
+for (const p in dataFiles) {
+
+}
 
 
 // debugging
-lg(dataFiles[0]["Name"]+ " : " +
-    new Date(
-        dataFiles[0]["B-day"]
-    ).toDateString()
-)
+
+
+// matching with data (nearest ~ furtherest)
+// R4
+// R6
+// R2
+// R5
+// R3
+// R1
