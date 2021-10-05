7da4830c109abc2024e89c480089b72c914941ab
NourAybak <nooreldin3000@gmail.com>
2021-10-05 19:12:52 +0200

    unknown undefined _55358_

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const getData = require('./lib/retrieve-data')
 const compareData = require('./lib/compare-bdays')
 
-console.log(compareData(getData()))
+console.log(
+    compareData(
+        getData()
+    )
+)
